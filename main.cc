bcce8695578ea336b36da9d60a0c653442513366
0xBAMA <jb239812@ohio.edu>
2025-11-12 13:42:50 -0500

    starting big from-scratch refactor

---
+++ main.cc
@@ -0,0 +1,21 @@
+#include "crystal.h"
+
+// master kill for all threads
+atomc< bool > threadKill = false;
+
+int main ( int argc, char* argv ) {
+
+    // Proc Updater Thread
+        // make sure the proc header has "previous" data
+
+    // Terminal UI Updater Thread
+        // CPU activity
+        // other central activity monitoring
+        // the active list of crystals
+            // option to add
+            // generate preview image on a given crystal
+            // automatically save out on completion
+
+    // something for rendering animations
+
+}
