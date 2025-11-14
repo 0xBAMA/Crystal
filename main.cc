5f2f2ee6b99a13d6b54b6b86bfd73b4ccc10fb8c
0xBAMA <jb239812@ohio.edu>
2025-11-13 20:13:50 -0500

    setup main threads (proc + terminal UI)

--- main.cc
+++ main.cc
@@ -1,27 +1,47 @@
 #include "crystal.h"
+#include "reporter.h"
 
 // master kill for all threads
 atomic< bool > threadKill = false;
 
 int main ( int argc, char** argv ) {
-
-    cout << "Hello World" << endl;
-
-    Crystal c;
-
-    cout << "Goodbye World" << endl;
-
     // Proc Updater Thread
         // make sure the proc header has "previous" data
+    updateProcData();
+    sleep_for( 5ms );
+    updateProcData();
 
-    // Terminal UI Updater Thread
-        // CPU activity
-        // other central activity monitoring
-        // the active list of crystals
-            // option to add
-            // generate preview image on a given crystal
-            // automatically save out on completion
+    // functionally a "service" thread, to keep the proc data updated
+    cout << "Spawning Proc Updater Thread... ";
+    std::thread procUpdaterThread = std::thread( [&]() {
+        while ( !threadKill ) {    
+            updateProcData();
+            sleep_for( 5ms );
+        }
+    });
+    cout << "Done." << endl;
 
-    // something for rendering animations
+    // tbd...
+    Crystal c;
 
+    cout << "Spawning Terminal UI Thread... ";
+	std::thread terminalUIThread = std::thread( [&] () {
+	 // this is the master thread
+        int iterations = 0;
+		auto tStart = high_resolution_clock::now();
+			
+        // while ( true ) {
+
+            // do the terminal UI shit
+                // CPU activity
+                // display table of Crystal activitybhgggghj
+        
+            // sleep_for( 100ms );            
+            // cout << "Running Monitor Thread " << std::chrono::duration_cast< std::chrono::milliseconds >( high_resolution_clock::now() - tStart )  << "ms" << endl;
+        // }
+    });            
+    cout << "Done." << endl;
+
+    procUpdaterThread.join();
+    terminalUIThread.join();
 }
